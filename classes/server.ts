33716eed3c02d472ec2d63c1972b7072a106b084
Lukas Santillán <lukassantillan2001@gmail.com>
2022-06-01 21:59:46 -0300

    completado server.ts

--- classes/server.ts
+++ classes/server.ts
@@ -1,1 +1,16 @@
 import express from 'express';
+import { SERVER_PORT } from '../global/environment';
+
+export class Servidor {
+    public app: express.Application;
+    public port: number;
+
+    public constructor(){
+        this.app = express();
+        this.port = SERVER_PORT;
+    }
+
+    start (callback: Function) {
+        this.app.listen(this.port, callback());
+    }
+}
