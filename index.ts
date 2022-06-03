e741c22212614887dc2406d669b47899d20f56a3
coordis 13a18 <cordis@puerta18.corp.ar>
2022-06-03 14:02:02 -0300

    server funcionando con un endpoint de prueba

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-const nombre = "Lukas";
-const apellido = "Santillán"
-console.log(`Me llamó: ${nombre} ${apellido}`);
+import {Servidor} from "./classes/server";
+import { router } from "./routes/router";
+import cors from "cors";
+
+let server = new Servidor();
+
+// Credenciales de seguridad
+server.app.use(cors({origin:true, credentials:true}));
+
+server.app.use('/', router)
+
+server.start( () => {
+    console.log("servidor corriendo en " + server.port);
+    
+});
