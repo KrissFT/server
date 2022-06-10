29299be868813fe1e0e0456b4ddd4e41150aa920
KrissFT <lukassantillan2001@gmail.com>
2022-06-10 13:55:17 -0300

    añadido bodyparser y pruebas de post

--- routes/router.ts
+++ routes/router.ts
@@ -9,3 +9,21 @@ router.get('/socies', (req: Request, res: Response) => {
         mensaje: "todo ok!"
     });
 });
+
+router.post('/registro', (req: Request, res: Response) => {
+    const nombre = req.body.nombre?req.body.nombre:null;
+
+    if(nombre===null){
+        res.json({
+            ok:false,
+            mensaje: "Nombre no recibido"
+        })
+    } else {
+        res.json({
+            ok:true,
+            mensaje: `Bienvenide ${nombre}!!!`
+        })            
+    }
+    
+    console.log(`Nombre: ${nombre}`)
+});
