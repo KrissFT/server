c89b67e259fd8a9f4b11b9a25bcebdcfb30b0405
unknown <usuario@PUERTA18.CORP.AR>
2022-06-22 13:50:08 -0300

    agregado readme y modelos

---
+++ readme.md
@@ -0,0 +1,4 @@
+<h1>servidor para proyecto de introducción a la programación de puerta18<h1>
+<h2>Para hacer<h2>
+investigar validación del lado del servidor
+qué hacer con los null
--- routes/router.ts
+++ routes/router.ts
@@ -1,22 +1,60 @@
 import { Router, Request, Response } from "express";
-
+import { Socie } from "../models/socie.model";
 export const router = Router();
 
 
 router.get('/socies', (req: Request, res: Response) => {
+    let socies: Array<Socie> = [
+        {
+            nombre: 'Lukas',
+            apellido: 'Santillán',
+            nick: 'N/A',
+            edad: 20
+        },
+        {
+            nombre: 'Irina',
+            apellido: 'Davico',
+            nick: 'Ir1n4',
+            edad: 21
+        },
+        {
+            nombre: 'John',
+            apellido: 'Maza',
+            nick: 'J0hny',
+            edad: 20
+        }
+    ];
     res.json({
         ok: true,
-        mensaje: "todo ok!"
+        socies: socies
     });
 });
 
 router.post('/registro', (req: Request, res: Response) => {
     let nombre:string
     let apellido:string
-    let fecha_de_nacimiento
+    let fecha_de_nacimiento:string
     let pronombre:string
     let documento:string
     let nickname:string
+
+    let direccion:string
+    let barrio:string
+    let localidad:string
+
+    let telefono:string
+    let redes:string
+    let correo:string
+
+    let escuela:string
+    let carrera:string
+
+    let nombreTutor:string
+    let apellidoTutor:string
+    let vinculo:string
+    let telefonoTutor:string
+    let documentoTutor:string
+    let edadTutor:string
 
     nombre = req.body.nombre?req.body.nombre:null
     apellido = req.body.apellido?req.body.apellido:null
@@ -24,7 +62,25 @@ router.post('/registro', (req: Request, res: Response) => {
     pronombre = req.body.pronoun?req.body.pronoun:null
     documento = req.body.dni?req.body.dni:null
     nickname = req.body.nick?req.body.nick:null
+
+    direccion = req.body.direccion?req.body.direccion:null
+    barrio = req.body.barrio?req.body.barrio:null
+    localidad = req.body.localidad?req.body.localidad:null
     
+    telefono = req.body.tel?req.body.tel:null
+    redes = req.body.redes?req.body.redes:null
+    correo = req.body.mail?req.body.mail:null
+
+    escuela = req.body.esc?req.body.esc:null
+    carrera = req.body.carrera?req.body.carrera:null
+
+    nombreTutor = req.body.nombreTutor?req.body.nombreTutor:null
+    apellidoTutor = req.body.apellidoTutor?req.body.apellidoTutor:null
+    vinculo = req.body.vinculo?req.body.vinculo:null
+    telefonoTutor = req.body.telTutor?req.body.telTutor:null
+    documentoTutor = req.body.dniTutor?req.body.dniTutor:null
+    edadTutor = req.body.edadTutor?req.body.edadTutor:null
+
     if (nombre !== null && apellido !== null && fecha_de_nacimiento !== null && pronombre !== null && documento !== null){
         console.log(`Nombre: ${nombre}`)
         console.log(`Apellido: ${apellido}`)
