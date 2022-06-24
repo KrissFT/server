d85a8b984a6ea03224bcc85042a5e942e8c2b81b
Lukas <usuario@PUERTA18.CORP.AR>
2022-06-24 13:50:57 -0300

    editado router para enviar ficha

--- routes/router.ts
+++ routes/router.ts
@@ -2,31 +2,45 @@ import { Router, Request, Response } from "express";
 import { Socie } from "../models/socie.model";
 export const router = Router();
 
+let socies: Array<Socie> = [
+    {
+        nombre: 'Lukas',
+        apellido: 'Santillán',
+        nick: 'Kriss',
+        edad: 20
+    },
+    {
+        nombre: 'Irina',
+        apellido: 'Davico',
+        nick: 'Ir1n4',
+        edad: 21
+    },
+    {
+        nombre: 'John',
+        apellido: 'Maza',
+        nick: 'J0hny',
+        edad: 20
+    }
+];
 
 router.get('/socies', (req: Request, res: Response) => {
-    let socies: Array<Socie> = [
-        {
-            nombre: 'Lukas',
-            apellido: 'Santillán',
-            nick: 'N/A',
-            edad: 20
-        },
-        {
-            nombre: 'Irina',
-            apellido: 'Davico',
-            nick: 'Ir1n4',
-            edad: 21
-        },
-        {
-            nombre: 'John',
-            apellido: 'Maza',
-            nick: 'J0hny',
-            edad: 20
-        }
-    ];
     res.json({
         ok: true,
         socies: socies
+    });
+});
+
+router.get('/socie/:nick', (req: Request, res: Response) => {
+    let ficha:Socie= {nombre: "", apellido: "", nick: "", edad:0 };
+
+    socies.forEach(socie => {
+        if (socie.nick === req.params.nick){
+            ficha = socie;
+        }
+    });
+    res.json({
+        ok: true,
+        socie: ficha
     });
 });
 
